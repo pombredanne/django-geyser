0e7ecacc31bbf88281faa520f3c811837db8a35f
James Lecker <james.lecker@mwcradio.com>
2010-06-24 13:15:42 -0500

    split fixtures into separate files

--- geyser/tests/permissions.py
+++ geyser/tests/permissions.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from django.core.management import call_command
 
 import authority
 
 from geyser.tests.base import GeyserTestCase
 from geyser.tests.testapp.models import TestModel1, TestModel2, TestModel3
+
 
 class PermissionTest(GeyserTestCase):
     def setUp(self):
@@ -12,13 +12,13 @@ class PermissionTest(GeyserTestCase):
             'testapp.testmodel1': {
                 'publish_to': ('testapp.testmodel2',),
                 'auto_perms': ('owner',),
-                'unique_for_date': ('name',),
             },
             'testapp.testmodel2': {
                 'publish_to': ('testapp.testmodel3',),
             }
         }
         authority.autodiscover()
+    
     def test_publishable_permission(self):
         permission_list = authority.sites.site.get_permissions_by_model(TestModel1)
         self.assertTrue(permission_list)
@@ -27,6 +27,7 @@ class PermissionTest(GeyserTestCase):
         self.assertEqual(permission.label, 'testmodel1_permission')
         self.assertTrue('publish_testmodel1' in permission.checks)
         self.assertEqual(len(permission.checks), 5)
+    
     def test_publication_permission(self):
         permission_list = authority.sites.site.get_permissions_by_model(TestModel3)
         self.assertTrue(permission_list)
@@ -35,6 +36,7 @@ class PermissionTest(GeyserTestCase):
         self.assertEqual(permission.label, 'testmodel3_permission')
         self.assertTrue('publish_testmodel2_to_testmodel3' in permission.checks)
         self.assertEqual(len(permission.checks), 5)
+    
     def test_both_permission(self):
         permission_list = authority.sites.site.get_permissions_by_model(TestModel2)
         self.assertTrue(permission_list)
@@ -44,5 +46,6 @@ class PermissionTest(GeyserTestCase):
         self.assertTrue('publish_testmodel2' in permission.checks)
         self.assertTrue('publish_testmodel1_to_testmodel2' in permission.checks)
         self.assertEqual(len(permission.checks), 6)        
+
 
 __all__ = ('PermissionTest',)
