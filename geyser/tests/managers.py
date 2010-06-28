949f75f9a029fdadbe807e55e739dae0c82c3d8d
James Lecker <james.lecker@mwcradio.com>
2010-06-28 12:50:27 -0500

    update manager tests

--- geyser/tests/managers.py
+++ geyser/tests/managers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.conf import settings
 
 from geyser.tests.base import GeyserTestCase
@@ -18,6 +20,10 @@ class ManagerTest(GeyserTestCase):
             },
             'testapp.testmodel2': {
                 'publish_to': ('testapp.testmodel3',),
+                'list_annotations': {
+                    'published': 'pub_date',
+                    'publication': 'pub_on'
+                }
             }
         }
         self.t1a = TestModel1.objects.get(pk=1)
@@ -31,17 +37,18 @@ class ManagerTest(GeyserTestCase):
         self.t2a_t3a = Droplet.objects.get(pk=4)
         self.t2a_t3b = Droplet.objects.get(pk=5)
     
-    def test_unfiltered(self):
+    def test_get_list(self):
+        # unfiltered
         all_pubs = Droplet.objects.get_list()
         self.assertEqual(all_pubs.count(), 5)
     
-    def test_by_publication(self):
+        # by publication
         to_3a = Droplet.objects.get_list(publications=self.t3a)
         self.assertTrue(self.t1a_t3a in to_3a)
         self.assertTrue(self.t2a_t3a in to_3a)
         self.assertEqual(len(to_3a), 2)
     
-    def test_by_publication_list(self):
+        # by publication list
         to_2a_or_3a = Droplet.objects.get_list(publications=[self.t2a, self.t3a])
         self.assertTrue(self.t1a_t2a in to_2a_or_3a)
         self.assertTrue(self.t1b_t2a in to_2a_or_3a)
@@ -49,30 +56,45 @@ class ManagerTest(GeyserTestCase):
         self.assertTrue(self.t2a_t3a in to_2a_or_3a)
         self.assertEqual(len(to_2a_or_3a), 4)                
     
-    def test_by_publishable_model(self):
+        # by publishable model
         t1_pubs = Droplet.objects.get_list(publishable_models=TestModel1)
         self.assertTrue(self.t1a_t2a in t1_pubs)
         self.assertTrue(self.t1b_t2a in t1_pubs)
         self.assertTrue(self.t1a_t3a in t1_pubs)
         self.assertEqual(len(t1_pubs), 3)
     
-    def test_by_publishable_model_list(self):
+        # by publishable model list
         t1_t2_pubs = Droplet.objects.get_list(publishable_models=[TestModel1, TestModel2])
         self.assertEqual(len(t1_t2_pubs), 5)
     
-    def test_by_publication_and_publishable_model(self):
+        # by publishable model and publication
         t1_to_2a = Droplet.objects.get_list(publishable_models=TestModel1, publications=self.t2a)
         self.assertTrue(self.t1a_t2a in t1_to_2a)
         self.assertTrue(self.t1b_t2a in t1_to_2a)
         self.assertEqual(len(t1_to_2a), 2)
-        
         t2_to_3a = Droplet.objects.get_list(publishable_models=TestModel2, publications=self.t3a)
         self.assertTrue(self.t2a_t3a in t2_to_3a)
         self.assertEqual(len(t2_to_3a), 1)
         
+        # by publishable list and publication
         t1_t2_to_3b = Droplet.objects.get_list(publishable_models=[TestModel1, TestModel2], publications=self.t3b)
         self.assertTrue(self.t2a_t3b in t1_t2_to_3b)
         self.assertEqual(len(t1_t2_to_3b), 1)
+    
+    def test_get_publishable_list(self):
+        # should not be able to pass list of publishables
+        self.assertRaises(TypeError,
+            Droplet.objects.get_publishable_list, publishable_model=[TestModel1, TestModel2])
+        
+        # by publication
+        t1_on_t2a = Droplet.objects.get_publishable_list(publishable_model=TestModel1, publications=self.t2a)
+        self.assertTrue(self.t1a in t1_on_t2a)
+        self.assertTrue(self.t1b in t1_on_t2a)
+        self.assertEqual(len(t1_on_t2a), 2)
+        self.assertTrue(isinstance(t1_on_t2a[0].published, datetime))
+        self.assertEqual(t1_on_t2a[0].publication, self.t2a)
+        self.assertTrue(isinstance(t1_on_t2a[1].published, datetime))
+        self.assertEqual(t1_on_t2a[1].publication, self.t2a)
 
 
 __all__ = ('ManagerTest',)
