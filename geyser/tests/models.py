5a8be419ff74833e14eb4a451a508230f9924455
James Lecker <james.lecker@mwcradio.com>
2010-06-29 14:58:37 -0500

    add published_by and updated_by to Droplet model

--- geyser/tests/models.py
+++ geyser/tests/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
 
 from geyser.tests.base import GeyserTestCase
 from geyser.tests.testapp.models import TestModel1, TestModel2
@@ -9,7 +10,7 @@ from geyser.models import Droplet
 
 
 class ModelTest(GeyserTestCase):
-    fixtures = ['test_objects.json']
+    fixtures = ['test_users.json', 'test_objects.json']
     
     def setUp(self):
         settings.GEYSER_PUBLISHABLES = {
@@ -21,20 +22,23 @@ class ModelTest(GeyserTestCase):
                 'publish_to': ('testapp.testmodel3',),
             }
         }
+        self.user = User.objects.get(pk=2)
         self.t1 = TestModel1.objects.get(pk=1)
         self.t2 = TestModel2.objects.get(pk=1)
     
     def test_remove_newest(self):
         droplet1 = Droplet(
             publishable=self.t1,
-            publication=self.t2
+            publication=self.t2,
+            published_by=self.user
         )
         droplet1.save()
         droplet1_updated = droplet1.updated
         self.assertTrue(droplet1.is_newest)
         droplet2 = Droplet(
             publishable=self.t1,
-            publication=self.t2
+            publication=self.t2,
+            published_by=self.user
         )
         droplet2.save()
         self.assertTrue(droplet2.is_newest)
