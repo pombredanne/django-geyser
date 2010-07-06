038957b3fa8906fe7758c5f78ee7b73352bfb125
James Lecker <james.lecker@mwcradio.com>
2010-07-06 14:32:13 -0500

    add publishing to view

--- geyser/tests/views.py
+++ geyser/tests/views.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 
 import authority
 
+from geyser.models import Droplet
 from geyser.tests.base import GeyserTestCase
 from geyser.tests.testapp.models import TestModel1, TestModel2, TestModel3
 
@@ -27,7 +28,7 @@ class ViewTest(GeyserTestCase):
         self.t3a = TestModel3.objects.get(pk=1)
         self.t3b = TestModel3.objects.get(pk=2)
     
-    def test_get_view(self):
+    def test_get(self):
         unauth_response = self.client.get('/t1/1/')
         self.assertEqual(unauth_response.status_code, 404)
         
@@ -56,7 +57,7 @@ class ViewTest(GeyserTestCase):
         self.assertEqual(forms[1].publication, self.t3b)
         self.assertEqual(forms[1].publication_type, self.type3)
     
-    def test_post_view(self):
+    def test_publish(self):
         unauth_response = self.client.post('/t1/1/')
         self.assertEqual(unauth_response.status_code, 404)
         
@@ -68,8 +69,78 @@ class ViewTest(GeyserTestCase):
         t1z_response = self.client.post('/t1/26/')
         self.assertEqual(t1z_response.status_code, 404)
         
-        t1a_response = self.client.post('/t1/1/')
+        form_dict = {
+            'form-TOTAL_FORMS': 2,
+            'form-INITIAL_FORMS': 2,
+            'form-0-publish': 'on',
+            'form-0-type': self.type3.id,
+            'form-0-id': self.t3a.id,
+            'form-1-type': self.type3.id,
+            'form-1-id': self.t3b.id
+        }
+        t1a_response = self.client.post('/t1/1/', form_dict)
+        
         self.assertEqual(t1a_response.status_code, 200)
+        self.assertEqual(t1a_response.context['object'], self.t1a)
+        self.assertTrue(isinstance(t1a_response.context['publication_formset'], BaseFormSet))
+        forms = t1a_response.context['publication_formset'].forms
+        self.assertEqual(len(forms), 2)
+        self.assertTrue(all([isinstance(form, Form) for form in forms]))
+        
+        self.assertEqual(forms[0].cleaned_data['id'], 1)
+        self.assertEqual(forms[0].cleaned_data['type'], self.type3.id)
+        self.assertEqual(forms[0].cleaned_data['publish'], True)
+        self.assertEqual(forms[1].cleaned_data['id'], 2)
+        self.assertEqual(forms[1].cleaned_data['type'], self.type3.id)
+        self.assertEqual(forms[1].cleaned_data['publish'], False)
+        
+        self.assertEqual(forms[0].publication, self.t3a)
+        self.assertEqual(forms[0].publication_type, self.type3)
+        self.assertEqual(forms[1].publication, self.t3b)
+        self.assertEqual(forms[1].publication_type, self.type3)
+        
+        published = Droplet.objects.get_list()
+        self.assertTrue(all(d.publishable == self.t1a for d in published))
+        self.assertTrue(any(d.publication == self.t3a for d in published))
+        self.assertEqual(len(published), 1)
+    
+    def atest_unpublish(self):
+        Droplet.objects.publish(self.t1a, self.t3a, self.user)
+        
+        self.client.login(username='user', password='')
+        
+        form_dict = {
+            'form-TOTAL_FORMS': 2,
+            'form-INITIAL_FORMS': 2,
+            'form-0-type': self.type3.id,
+            'form-0-id': self.t3a.id,
+            'form-1-type': self.type3.id,
+            'form-1-id': self.t3b.id
+        }
+        t1a_response = self.client.post('/t1/1/', form_dict)
+        
+        self.assertEqual(t1a_response.status_code, 200)
+        self.assertEqual(t1a_response.context['object'], self.t1a)
+        self.assertTrue(isinstance(t1a_response.context['publication_formset'], BaseFormSet))
+        forms = t1a_response.context['publication_formset'].forms
+        self.assertEqual(len(forms), 2)
+        self.assertTrue(all([isinstance(form, Form) for form in forms]))
+        
+        self.assertEqual(forms[0].cleaned_data['id'], 1)
+        self.assertEqual(forms[0].cleaned_data['type'], self.type3.id)
+        self.assertEqual(forms[0].cleaned_data['publish'], False)
+        self.assertEqual(forms[1].cleaned_data['id'], 2)
+        self.assertEqual(forms[1].cleaned_data['type'], self.type3.id)
+        self.assertEqual(forms[1].cleaned_data['publish'], False)
+        
+        self.assertEqual(forms[0].publication, self.t3a)
+        self.assertEqual(forms[0].publication_type, self.type3)
+        self.assertEqual(forms[1].publication, self.t3b)
+        self.assertEqual(forms[1].publication_type, self.type3)
+        
+        published = Droplet.objects.get_list()
+        self.assertEqual(len(published), 0)
+
 
 
 __all__ = ('ViewTest',)
--- geyser/views.py
+++ geyser/views.py
@@ -25,18 +25,37 @@ class PublishObject(object):
             raise Http404
         
         publication_types = []
-        allowed_by_ids = []
+        formset_data = []
         for publication in publications:
             publication_type = ContentType.objects.get_for_model(publication)
             publication_types.append(publication_type)
-            allowed_by_ids.append({'type': publication_type.id, 'id': publication.id})
-                
-        publication_formset = PublishFormSet(initial=allowed_by_ids)
+            formset_data.append({
+                'type': publication_type.id,
+                'id': publication.id,
+                'publish': False
+            })
+        
+        if request.method == 'POST':
+            publication_formset = PublishFormSet(request.POST, initial=formset_data)
+        else:
+            publication_formset = PublishFormSet(initial=formset_data)
+        
         for (form, publication, publication_type) in \
                 zip(publication_formset.forms, publications, publication_types):
             form.publication = publication
             form.publication_type = publication_type
         
+        if publication_formset.is_valid():
+            to_publish = []
+            to_unpublish = []
+            for form in publication_formset.forms:
+                if form.changed_data == ['publish']:
+                    if form.cleaned_data['publish']:
+                        to_publish.append(form.publication)
+                    else:
+                        to_unpublish.append(form.publication)
+            Droplet.objects.publish(publishable, to_publish, request.user)
+                 
         return render_to_response(
             'geyser/publish.html',
             {
