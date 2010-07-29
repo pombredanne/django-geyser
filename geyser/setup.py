4ab06188be3b98fcc75c0c9fa4ddd27c1177a310
James Lecker <james.lecker@mwcradio.com>
2010-07-29 11:27:36 -0500

    add setup.py

---
+++ geyser/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-geyser',
+    version='0.1',
+    description='A Django application which allows objects of any type to be published to objects of any other type.',
+    author='James Lecker Jr',
+    author_email='james@jameslecker.com',
+    url='http://github.com/jlecker/django-geyser',
+    packages=['geyser']
+)
